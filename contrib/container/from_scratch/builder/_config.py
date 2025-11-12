67871d8c1594ec391ef3967001e9dbce059725c6
Brent Williams <jbwtech@gmail.com>
2025-11-12 02:28:20 +0000

    Moves these constants to the class definition

--- contrib/container/from_scratch/builder/_config.py
+++ contrib/container/from_scratch/builder/_config.py
@@ -1,14 +1,4 @@
 
-# Container names
-IMAGE_BASE = "base"      # OS and packages
-IMAGE_SAGE = "sage"      # Sage without extra packages
-IMAGE_PRECELL = "precell"    # Everything but SageCell and system configuration
-IMAGE_SAGECELL = "sagecell"      # Sage and SageCell
-IMAGE_BACKUP = "sagecell-backup"     # Saved master for restoration if necessary
-IMAGE_TESTER = "sctest"  # Accessible via special port, for testing
-IMAGE_PREFIX = "sc-"     # Prefix for main compute nodes
-
-
 # Theses are the lists of packages to install
 filelist = [
     "lists/base/prerequisites.txt",
